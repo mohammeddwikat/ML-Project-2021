4aaa705937f2d82b8f2665395eb12440273b4183
Mohammad Dwikat <mohammed.dwikate1999@gmail.com>
2021-12-03 22:56:27 +0200

    master : finsih little part1 :)

--- part1/predictionModelPart1.py
+++ part1/predictionModelPart1.py
@@ -3,21 +3,24 @@ from sklearn.tree import DecisionTreeRegressor
 from sklearn.linear_model import LinearRegression
 import joblib
 from main import features, coronaCases
+from sklearn.model_selection import cross_val_score
 
 # split data to training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(features, coronaCases, test_size=0.25, random_state=0)
+X_train, X_test, y_train, y_test = train_test_split(features, coronaCases, test_size=0.25)
 
 # Decision Tree Predictor Model
-DT_model = DecisionTreeRegressor().fit(X_train, y_train)
+DT_model = DecisionTreeRegressor(random_state=0).fit(X_train, y_train)
 DT_predict = DT_model.predict(X_test)
 print("Decision Tree Score using R^2:", DT_model.score(X_test, y_test))
+print("Decision Tree Score using cross validation", cross_val_score(DT_model, features, coronaCases, cv=5).mean())
 
 # Linear Regression
 reg = LinearRegression().fit(X_train, y_train)
 y_predict = reg.predict(X_test)
 print("Regression model score using R^2:", reg.score(X_test, y_test))
+print("Regression model Score using cross validation", cross_val_score(reg, features, coronaCases, cv=5).mean())
 
-
+# Save models
 filename = "regressionModel.joblib"
 joblib.dump(reg, filename)
 filename = "DecisionTreeModel.joblib"
--- part1/test models.py
+++ part1/test models.py
@@ -1,28 +1,38 @@
 from joblib import load
-import matplotlib.pyplot as plt
-from sklearn.datasets import load_iris
 from sklearn import tree
+from sklearn.datasets import load_iris
+import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import cross_val_score
-from main import features, coronaCases
+from main import coronaCases, features, trainingData, labels
+from sklearn import svm
 
 DT_model = load("../Models/DecisionTreeModel.joblib")
 reg = load("../Models/regressionModel.joblib")
 
 # get test and training data
-_, X_test, _, y_test = train_test_split(features, coronaCases, test_size=0.25)
+_, X_test, __, y_test = train_test_split(features, coronaCases, test_size=0.25)
 
-# # Models score using cross validation
-# scores = cross_val_score(DT_model, features, coronaCases, cv=5)
-# print("Decision Tree Score using cross validation:", sum(scores)/len(scores))
-# scoresReg = cross_val_score(reg, features, coronaCases, cv=5)
-# print("Regression model Score using cross validation:", sum(scoresReg)/len(scoresReg))
+# Models score using cross validation
+scores = cross_val_score(DT_model, features, coronaCases, cv=5)
+print("Decision Tree Score using cross validation:", scores.mean())
+scores2 = cross_val_score(reg, features, coronaCases, cv=5)
+print("Regression model Score using cross validation:", scores2.mean())
 
 # Models score using R^2
 print("Decision Tree Score using R^2:", DT_model.score(X_test, y_test))
 print("Regression model score using R^2:", reg.score(X_test, y_test))
 
+# index = 64
+# print((trainingData.iloc[:, range(0,5)]).iloc[index])
+# print(DT_model.predict([((trainingData.loc[:, list(labels)[5:15]]).iloc[index])]))
+# print(reg.predict([((trainingData.loc[:, list(labels)[5:15]]).iloc[index])]))
 
+# coefficients for linear regression
+print(reg.coef_)
+print(reg.intercept_)
+
+# Visualize the decision tree
 iris = load_iris()
 X, y = iris.data, iris.target
 tree.plot_tree(DT_model)
