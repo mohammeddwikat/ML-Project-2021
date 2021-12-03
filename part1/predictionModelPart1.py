b7b8ec2dbb07a638669fbd65453507c5283b751c
Mohammad Dwikat <mohammed.dwikate1999@gmail.com>
2021-12-03 23:29:02 +0200

    master : build regressor model using KNN

--- part1/predictionModelPart1.py
+++ part1/predictionModelPart1.py
@@ -4,6 +4,8 @@ from sklearn.linear_model import LinearRegression
 import joblib
 from main import features, coronaCases
 from sklearn.model_selection import cross_val_score
+from sklearn.neighbors import KNeighborsRegressor
+
 
 # split data to training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(features, coronaCases, test_size=0.25)
@@ -20,8 +22,16 @@ y_predict = reg.predict(X_test)
 print("Regression model score using R^2:", reg.score(X_test, y_test))
 print("Regression model Score using cross validation", cross_val_score(reg, features, coronaCases, cv=5).mean())
 
+# K-Nearest Regressor
+neigh = KNeighborsRegressor(n_neighbors=11)
+neigh.fit(X_train, y_train)
+print("K-Nearest Regressor model score using R^2:", cross_val_score(neigh, features, coronaCases, cv=5).mean())
+print("K-Nearest Regressor model score using cross validation:", neigh.score(X_test, y_test))
+
 # Save models
 filename = "regressionModel.joblib"
 joblib.dump(reg, filename)
 filename = "DecisionTreeModel.joblib"
 joblib.dump(DT_model, filename)
+filename = "K-NearestModel.joblib"
+joblib.dump(neigh, filename)
--- part1/test models.py
+++ part1/test models.py
@@ -5,10 +5,10 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import cross_val_score
 from main import coronaCases, features, trainingData, labels
-from sklearn import svm
 
 DT_model = load("../Models/DecisionTreeModel.joblib")
 reg = load("../Models/regressionModel.joblib")
+knn = load("../Models/K-NearestModel.joblib")
 
 # get test and training data
 _, X_test, __, y_test = train_test_split(features, coronaCases, test_size=0.25)
@@ -16,12 +16,15 @@ _, X_test, __, y_test = train_test_split(features, coronaCases, test_size=0.25)
 # Models score using cross validation
 scores = cross_val_score(DT_model, features, coronaCases, cv=5)
 print("Decision Tree Score using cross validation:", scores.mean())
-scores2 = cross_val_score(reg, features, coronaCases, cv=5)
-print("Regression model Score using cross validation:", scores2.mean())
+scores = cross_val_score(reg, features, coronaCases, cv=5)
+print("Regression model Score using cross validation:", scores.mean())
+scores = cross_val_score(knn, features, coronaCases, cv=5)
+print("KNN regressor model Score using cross validation:", scores.mean())
 
 # Models score using R^2
 print("Decision Tree Score using R^2:", DT_model.score(X_test, y_test))
 print("Regression model score using R^2:", reg.score(X_test, y_test))
+print("KNN regressor model score using R^2:", knn.score(X_test, y_test))
 
 # index = 64
 # print((trainingData.iloc[:, range(0,5)]).iloc[index])
